3feb63e275eedb44f7c537c62ed66184d268f6f0
Philip Linden <lindenphilipj@gmail.com>
2024-05-27 03:27:53 -0400

    start replacing placeholders

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Philip Linden | Space Systems Engineer, Open Lunar Fellow, Microgravity Enjoyer';
+export const SITE_DESCRIPTION = 'Phil is a spacecraft engineer, writer, and all around space nerd. His core values are: Do Good, Be Collaborative, Dream Big, Strive for Openness, and Take Pride in Every Task.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
